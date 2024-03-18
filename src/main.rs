47a054adb19c646fe7eb5c14d6ec1756b6f95a0b
jdrew153 <jdrew153@gmail.com>
2024-03-18 16:18:12 -0400

    cors stuff

--- src/main.rs
+++ src/main.rs
@@ -71,9 +71,14 @@ fn index() -> String {
     return "Howdy".to_string()
 }
 
+#[options("/<_..>")]
+fn all_options() {
+    /* Intentionally left empty */
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(CORS)
-        .mount("/", routes![index, create_message, delete_message_handler, read_all_messages])
+        .mount("/", routes![all_options, index, create_message, delete_message_handler, read_all_messages])
 }
