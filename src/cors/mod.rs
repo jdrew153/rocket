056b4eef6e5fd758e89ab18dfc332872d1e9cdb2
jdrew153 <jdrew153@gmail.com>
2024-03-18 16:11:15 -0400

    cors stuff

--- src/cors/mod.rs
+++ src/cors/mod.rs
@@ -26,7 +26,7 @@ impl Fairing for CORS {
 
         response.set_header(Header::new(
             "Access-Control-Allow-Origin",
-            "*",
+            "https://scylladbmessenger.vercel.app/",
         ));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
     }
